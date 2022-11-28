995984fdfdc4532e015103416daa0bb2954016e5
Vagos Labrou <e.lamprou@upnet.gr>
2022-11-28 20:53:26 +0200

    added benchmark run.sh

--- ex1/src/diffusion/run.sh
+++ ex1/src/diffusion/run.sh
@@ -8,13 +8,26 @@
 
 D=1
 L=1
-N=1024
+N=128
 T=1000
 DT=1e-9
 
 # DEBUG="alacritty -e -e ./diffusion2d_mpi $D $L $N $T $DT"
 # DEBUG="valgrind --leak-check=yes"
+# RUN_CMD="export OMP_NUM_THREADS=4; ./diffusion2d_openmp $D $L $N $T $DT"
 ARGS="--mca opal_warn_on_missing_libcuda 0 --use-hwthread-cpus"
 
-# export OMP_NUM_THREADS=4; ./diffusion2d_openmp $D $L $N $T $DT
-mpirun -n 4 $ARGS ./diffusion2d_mpi $D $L $N $T $DT 
+benchmark() 
+{
+    N=$1
+    RUN_CMD="mpirun -n 1 $ARGS ./diffusion2d_mpi $D $L $N $T $DT" 
+    
+    $RUN_CMD
+}
+
+SIZES=(1024 2048 4096)
+
+for N in ${SIZES[@]}; do
+  echo "Running mpi code with N=$N..."
+  benchmark $N
+done
