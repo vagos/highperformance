62cbb274cc30b6c8afa46876b300b2734cc8e5b3
Evangelos Labrou <e.lamprou@upnet.gr>
2023-02-20 15:03:42 +0200

    removed merge sort (too slow)

--- project/myknn-main/myknn-omp.c
+++ project/myknn-main/myknn-omp.c
@@ -16,52 +16,6 @@ static double ydata[TRAINELEMS];
 
 #define MAX_NNB	256
 
-void merge(double *arr, int *arr_x, int left, int mid, int right) {
-    int i, j, k;
-    double *temp = (double*)malloc((right - left + 1) * sizeof(double));
-    int *temp_x = (int*)malloc((right - left + 1) * sizeof(int));
-    i = left; j = mid + 1; k = 0;
-    while (i <= mid && j <= right) {
-        if (arr[i] < arr[j]) {
-            temp[k] = arr[i];
-            temp_x[k] = arr_x[i];
-            k++; i++;
-        } else {
-            temp[k] = arr[j];
-            temp_x[k] = arr_x[j];
-            k++; j++;
-        }
-    }
-    while (i <= mid) {
-        temp[k] = arr[i];
-        temp_x[k] = arr_x[i];
-        k++; i++;
-    }
-    while (j <= right) {
-        temp[k] = arr[j];
-        temp_x[k] = arr_x[j];
-        k++; j++;
-    }
-    for (i = left; i <= right; i++) {
-        arr[i] = temp[i - left];
-        arr_x[i] = temp_x[i - left];
-    }
-    free(temp);
-    free(temp_x);
-}
-
-void mergesort(double *arr, int *arr_x, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        #pragma omp task shared(arr, arr_x)
-        mergesort(arr, arr_x, left, mid);
-        #pragma omp task shared(arr, arr_x)
-        mergesort(arr, arr_x, mid + 1, right);
-        #pragma omp taskwait
-        merge(arr, arr_x, left, mid, right);
-    }
-}
-
 void compute_knn_brute_force(double **xdata, double *q, int npat, int lpat, int knn, int *nn_x, double *nn_d)
 {
 	int i, max_i;
