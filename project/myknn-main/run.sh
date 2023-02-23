d54bf7498c71feeecfdc427ca19a1df7cb68f7d6
Gpanaritis <jpanaritis2001@gmail.com>
2023-02-23 12:20:16 +0200

    Updated makefile and run.sh

--- project/myknn-main/run.sh
+++ project/myknn-main/run.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-./myknn-serial x.txt q.txt
-./myknn-omp x.txt q.txt
+echo "My knn Serial" > serial.txt
+echo " " >> serial.txt
+./myknn-serial x.txt q.txt >> serial.txt
+echo " " >> serial.txt
+echo "My knn Serial Alt" >> serial.txt
+echo " " >> serial.txt
+./myknn-serial-alt x.txt q.txt >> serial.txt
+
+echo "My knn OpenMP" > omp.txt
+echo " " >> omp.txt
+for i in {1,2,4,6,8,12,16,32}
+do
+    echo "OMP_NUM_THREADS=$i" >> omp.txt
+    echo " " >> omp.txt
+    OMP_NUM_THREADS= $i ./myknn-omp x.txt q.txt >> omp.txt
+    echo " " >> omp.txt
+    echo " " >> omp.txt
+done
+
+# ./myknn-serial x.txt q.txt
+# ./myknn-omp x.txt q.txt
